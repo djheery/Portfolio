acb51ee3b57cd2e31af5c4ca6be0e30e76442826
Daniel Heery <danieljheery@gmail.com>
2023-07-23 15:51:40 +0100

    Portfolio init

---
+++ app/portfolio/page.tsx
@@ -0,0 +1,15 @@
+import styles from './page.module.css';
+
+/**
+ * The functional component for the Portfolio Page
+*/
+
+const PortfolioPage: React.FC = () => {
+  return (
+    <div>
+      PortfolioPage Component
+    </div>
+  )
+}
+
+export default PortfolioPage;
