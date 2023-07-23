8a7b3a471547e4b85306a748bf3537959a4d4375
Daniel Heery <danieljheery@gmail.com>
2023-07-23 15:19:22 +0100

    Site init ;

---
+++ app/components/Header/Header.tsx
@@ -0,0 +1,17 @@
+import styles from 'Header.module.css';
+
+/**
+ * The functional component for Header
+ *
+ * @param myParam your params here
+*/
+
+const Header: React.FC = () => {
+  return (
+    <div>
+      <h1>Header</h1>
+    </div>
+  )
+}
+
+export default Header;
