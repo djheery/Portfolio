8d986f8e393d567830042ac27638962944d76afc
Daniel Heery <danieljheery@gmail.com>
2023-08-24 13:39:20 +0100

    Setting up Jest for testing

--- app/portfolio/(mini-projects)/sorting-algorithm-visualizer/util/algorithms/visual/radix-sort-visual.ts
+++ app/portfolio/(mini-projects)/sorting-algorithm-visualizer/util/algorithms/visual/radix-sort-visual.ts
@@ -1,13 +1,15 @@
 import { SortItemArray } from "../../../models/sort-models";
 
-/**
- * Describe your method...
- *
- * @param: paramName This param represents...
- * @returns: This method returns...
-*/
-
-
 export function* radixSortVisual(arr: SortItemArray) {
+  let max = getMax(arr); 
 
+}
+
+function getMax(arr: SortItemArray): number {
+  let max = arr[0][0];
+  for(let i = 1; i < arr.length; i++) {
+    if(arr[i][0] > max) max = arr[i][0]; 
+  };
+
+  return max; 
 }
