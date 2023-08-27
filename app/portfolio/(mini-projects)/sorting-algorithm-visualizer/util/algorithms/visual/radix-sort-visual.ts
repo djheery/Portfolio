4af5d370ff5d8783a4540357d9cab8a5abfb50d1
Daniel Heery <danieljheery@gmail.com>
2023-08-27 10:25:15 +0100

    Begginning Radix Sort

--- app/portfolio/(mini-projects)/sorting-algorithm-visualizer/util/algorithms/visual/radix-sort-visual.ts
+++ app/portfolio/(mini-projects)/sorting-algorithm-visualizer/util/algorithms/visual/radix-sort-visual.ts
@@ -2,7 +2,18 @@ import { SortItemArray } from "../../../models/sort-models";
 
 export function* radixSortVisual(arr: SortItemArray) {
   let max = getMax(arr); 
+  for(let i = 0; i < max; i++) {
+    let buckets = Array.from({length: 10}, () => []); 
 
+    for(let j = 0; j < arr.length; j++) {
+      let didgit = getDigit(arr[i][0], i); 
+
+    };
+  };
+}
+
+function getDigit(num: number, idx: number) {
+  return Math.floor(Math.abs(num) / Math.pow(10, idx)) % 10;  
 }
 
 function getMax(arr: SortItemArray): number {
@@ -11,5 +22,5 @@ function getMax(arr: SortItemArray): number {
     if(arr[i][0] > max) max = arr[i][0]; 
   };
 
-  return max; 
+  return String(max).length; 
 }
