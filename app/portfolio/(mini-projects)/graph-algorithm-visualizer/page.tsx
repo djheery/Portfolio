d66955a2280bc0f5f786e8b8145a3f07210b7dbc
Daniel Heery <danieljheery@gmail.com>
2023-10-18 10:40:39 +0100

    styles

--- app/portfolio/(mini-projects)/graph-algorithm-visualizer/page.tsx
+++ app/portfolio/(mini-projects)/graph-algorithm-visualizer/page.tsx
@@ -2,6 +2,7 @@ import ContentContainer from "@/app/components/Containers/ContentContainer/Conte
 import FullScreenContainer from "@/app/components/Containers/FullScreenContainer/FullScreenContainer";
 import NoSSRWrapper from "@/app/components/Containers/NoSSRWrapper/NoSSRWrapper";
 import { Suspense } from "react";
+import styles from "./page.module.css"
 
 /**
  * The functional component for the Graph Algorithm Visualizer Page.  
